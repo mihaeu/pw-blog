fc78836841e8273965b5c801d3d1f41ffa5b4a48
Michael Haeuslmann <haeuslmann@gmail.com>
2015-10-26 01:52:35 +0100

    Added grunt watch to automatically run tests on save.

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        phpunit: {
+            classes: {
+                dir: 'tests/'
+            },
+            options: {
+                bin: 'phpdbg -qrr vendor/bin/phpunit',
+                configuration: 'phpunit.xml.dist',
+                colors: true
+            }
+        },
+        watch: {
+            test: {
+                files: ['src/**/*.php'],
+                tasks: ['phpunit']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-phpunit');
+    grunt.loadNpmTasks('grunt-notify');
+
+    grunt.registerTask('default', ['watch']);
+
+};
