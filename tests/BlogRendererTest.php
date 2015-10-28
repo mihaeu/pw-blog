9eaaae0a7a54ea8fd4d3a120e3cc4565d8cf0b67
Michael Haeuslmann <haeuslmann@gmail.com>
2015-10-28 18:14:20 +0100

    Added another test for blog render.

--- tests/BlogRendererTest.php
+++ tests/BlogRendererTest.php
@@ -47,6 +47,35 @@ class BlogRendererTest extends \PHPUnit_Framework_TestCase
             .'Bla ....'.PHP_EOL;
         $this->assertEquals($expected, $this->mockConsole->output);
     }
+
+    public function testRendersManyArticles()
+    {
+        $article1 = new Article('Test Headline!', 'Bla ....');
+        $article1->addTag(new Tag("Tag1"));
+        $article1->addTag(new Tag("Tag2"));
+        $article2 = new Article('Test Headline!', 'Bla ....');
+        $article2->addTag(new Tag("Tag1"));
+        $article2->addTag(new Tag("Tag2"));
+
+        $owner = new User('owner@blog.de');
+        $blog = new Blog('My Blog', $owner);
+
+        $blog->publishArticleFromUser($article1, $owner);
+        $blog->publishArticleFromUser($article2, $owner);
+
+        $this->blogRenderer->render($blog);
+
+        $expected =
+            'My Blog'.PHP_EOL
+            .PHP_EOL
+            .'Test Headline!'.PHP_EOL
+            .'Tag1, Tag2'.PHP_EOL
+            .'Bla ....'.PHP_EOL
+            .'Test Headline!'.PHP_EOL
+            .'Tag1, Tag2'.PHP_EOL
+            .'Bla ....'.PHP_EOL;
+        $this->assertEquals($expected, $this->mockConsole->output);
+    }
 }
 
 class MockConsole extends Console
